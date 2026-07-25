d83a4fa1eb1e340d99e3f0cd32cef26d86cc7fca
Ian Munashe <macmushe@gmail.com>
2026-07-25 18:58:03 +0200

    Update sessionOptions to disable secure cookies for testing purposes, ensuring compatibility during development.

--- src/session-options.ts
+++ src/session-options.ts
@@ -2,14 +2,12 @@ import { SessionOptions } from "iron-session";
 
 import { Utils } from "./lib/utils";
 
-const isHttps = process.env.NEXT_PUBLIC_APP_URL?.startsWith("https://") ?? false;
-
 export const sessionOptions: SessionOptions = {
   password: String(process.env.NEXT_AUTH_SECRET),
   cookieName: String(process.env.NEXT_COOKIE_NAME),
   cookieOptions: {
     httpOnly: true,
-    secure: isHttps, //TODO: This is only for testing
+    secure: false, //TODO: This is only for testing
     // secure: process.env.NODE_ENV === "production",
     maxAge: Utils.instance.toSeconds(Utils.instance.futureDateTime("90d")),
     sameSite: "lax",
