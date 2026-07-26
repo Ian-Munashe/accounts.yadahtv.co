98e9328e8f44ac0ca5a21e0d4f87d30aefd66bfa
Ian Munashe <macmushe@gmail.com>
2026-07-26 23:23:13 +0200

    Update Next.js configuration to include allowedDevOrigins, increment package version to 1.0.5, modify dev script, and remove withCredentials from axios hook for improved session management.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "standalone"
+  output: "standalone",
+  allowedDevOrigins: ["10.10.1.5"],
 };
 
 export default nextConfig;
